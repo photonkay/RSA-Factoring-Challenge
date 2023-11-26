bd249c4841ad323e06baaab7ceba1aca511fd01b
Prince <princeasa2005@gmail.com>
2023-11-26 23:57:12 +0000

    Break the code

---
+++ factors.c
@@ -0,0 +1,58 @@
+#include "factors.h"
+
+/**
+ * factorize - factors number and prints result
+ * @n: number to factorize
+ * Return: void
+ */
+void factorize(long long int n)
+{
+	long long int i;
+
+	for (i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			printf("%lld=%ld*%ld\n", n, n/i, i);
+			break;
+		}
+	}
+}
+
+/**
+ * main - main entry point of factoring code
+ * @argc: command line arg count
+ * @argv: command line arg array
+ * Return: 0 on  success
+ */
+int main(int argc, char *argv[])
+{
+	char *filename;
+	FILE *file;
+	char line[512];
+	int num;
+
+	if (argc !=2)
+	{
+		fprintf(stderr, "Usage: ./factors <file>\n");
+		exit(EXIT_FAILURE);
+	}
+
+	filename = argv[1];
+	file = fopen(filename, "r");
+
+	if (!file)
+	{
+		perror("Error opening file");
+		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(line, sizeof(line), file))
+	{
+		num = atoi(line);
+		factorize(num);
+	}
+
+	fclose(file);
+	return (0);
+}
---
+++ factors.h
@@ -0,0 +1,11 @@
+#ifndef _FACTOR_H
+#define _FACTOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void factorize(long long int n);
+
+#endif /* _FACTOR_H */
